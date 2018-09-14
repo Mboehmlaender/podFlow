ddd76e34d23d9cd3475d435d6da72f2d5acb01d5
Mboehmlaender <41646409+Mboehmlaender@users.noreply.github.com>
2018-09-14 22:03:24 +0200

    dbsettings_sample.php
    The file must be renamed to dbsettings.php!

---
+++ config/dbsettings _sample.php
@@ -0,0 +1,9 @@
+<?php
+
+						define('DB_HOST','localhost');
+						define('DB_USER','root');
+						define('DB_PASSWORD','rabenNest7$');
+						define('DB_NAME','podflow_dev');
+						define('DB_PREFIX','');
+				 
+					 ?>
