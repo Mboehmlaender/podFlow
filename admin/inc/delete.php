559166ac7f8b8701ef147cf7c5c25379e2994b60
Mboehmlaender <41646409+Mboehmlaender@users.noreply.github.com>
2019-03-26 14:55:39 +0100

    Update Templates

--- admin/inc/delete.php
+++ admin/inc/delete.php
@@ -8,17 +8,45 @@ if(isset($_POST)){
 	
 	//Kategorie löschen
 	if(isset($_GET['del_category'])){
+		
+		$sql_select_templates = "SELECT * FROM ".DB_PREFIX."episode_templates";
+		$sql_select_templates_result = mysqli_query($con, $sql_select_templates);
+		while($sql_select_templates_row = mysqli_fetch_assoc($sql_select_templates_result))
+		{
+			$array = explode(',', $sql_select_templates_row['CATEGORIES']);
+			$array = array_diff($array, [$_POST['cat_id']]);
+			$array_new = implode(',', $array);
+			
+			$sql_update_template = "UPDATE ".DB_PREFIX."episode_templates SET CATEGORIES = '".$array_new."' WHERE ID = ".$sql_select_templates_row['ID'];
+			mysqli_query($con, $sql_update_template);
+		}
+		
+				
+		
 		$sql_delete_category = "DELETE FROM ".DB_PREFIX."categories WHERE ID = ".$_POST['cat_id'].";";
 		$sql_delete_category .= "DELETE FROM ".DB_PREFIX."episode_categories WHERE ID_CATEGORY = ".$_POST['cat_id'].";";
 		$sql_delete_category .= "DELETE FROM ".DB_PREFIX."links WHERE ID_CATEGORY = ".$_POST['cat_id'].";";
 		$sql_delete_category .= "DELETE FROM ".DB_PREFIX."topics WHERE ID_CATEGORY = ".$_POST['cat_id'];
-		mysqli_multi_query($con, $sql_delete_category); 
-		echo $sql_delete_category;
+ 		mysqli_multi_query($con, $sql_delete_category); 
+ 		echo $sql_delete_category;
 		return;
 	}
 
 	//Benutzer löschen
 	if(isset($_GET['delete_user'])){
+		
+		$sql_select_templates = "SELECT * FROM ".DB_PREFIX."episode_templates";
+		$sql_select_templates_result = mysqli_query($con, $sql_select_templates);
+		while($sql_select_templates_row = mysqli_fetch_assoc($sql_select_templates_result))
+		{
+			$array = explode(',', $sql_select_templates_row['USERS']);
+			$array = array_diff($array, [$_POST['user_id']]);
+			$array_new = implode(',', $array);
+			
+			$sql_update_template = "UPDATE ".DB_PREFIX."episode_templates SET USERS = '".$array_new."' WHERE ID = ".$sql_select_templates_row['ID'];
+			mysqli_query($con, $sql_update_template);
+		}
+		
 		$sql_delete_user = "DELETE FROM ".DB_PREFIX."users WHERE ID = ".$_POST['user_id'].";";
 		$sql_delete_user .= "DELETE FROM ".DB_PREFIX."topics WHERE ID_USER = ".$_POST['user_id'].";";
 		$sql_delete_user .= "DELETE FROM ".DB_PREFIX."links WHERE ID_USER = ".$_POST['user_id'].";";
