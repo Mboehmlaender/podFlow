c2aada07bbb2da84ec4d9039c8321c1f68aa9163
Mboehmlaender <41646409+Mboehmlaender@users.noreply.github.com>
2019-04-26 10:48:59 +0200

    Update insert.php

--- inc/insert.php
+++ inc/insert.php
@@ -17,7 +17,7 @@ if(isset($_POST)){
 		$category = mysqli_real_escape_string($con,$_POST['category']);
 		$link_descr = mysqli_real_escape_string($con,$_POST['link_descr']);
 		$link_url = mysqli_real_escape_string($con,$_POST['link_url']);
-		$sql_select_max_reihenf = "SELECT MAX(REIHENF) AS MAX FROM (SELECT REIHENF FROM topics WHERE ID_EPISODE = '".$_SESSION['cur_episode']."' AND ID_PODCAST = '".$_SESSION['podcast']."' AND ID_CATEGORY ='".$category."' UNION ALL SELECT REIHENF FROM links WHERE ID_EPISODE = '".$_SESSION['cur_episode']."' AND ID_PODCAST = '".$_SESSION['podcast']."' AND ID_CATEGORY ='".$category."') AS QUERY";
+		$sql_select_max_reihenf = "SELECT MAX(REIHENF) AS MAX FROM (SELECT REIHENF FROM ".DB_PREFIX."topics WHERE ID_EPISODE = '".$_SESSION['cur_episode']."' AND ID_PODCAST = '".$_SESSION['podcast']."' AND ID_CATEGORY ='".$category."' UNION ALL SELECT REIHENF FROM ".DB_PREFIX."links WHERE ID_EPISODE = '".$_SESSION['cur_episode']."' AND ID_PODCAST = '".$_SESSION['podcast']."' AND ID_CATEGORY ='".$category."') AS QUERY";
  		$sql_select_max_result = mysqli_query($con, $sql_select_max_reihenf);
 		$row = mysqli_fetch_assoc($sql_select_max_result);
 		$row_new = 1 + $row['MAX'];
@@ -57,7 +57,7 @@ if(isset($_POST)){
 		$url = mysqli_real_escape_string($con,$_POST['url']);
 		$category = mysqli_real_escape_string($con,$_POST['category']);
 
-		$sql_select_max_reihenf = "SELECT MAX(REIHENF) AS MAX FROM (SELECT REIHENF FROM ".DB_PREFIX."topics WHERE ID_EPISODE = '".$_SESSION['cur_episode']."' AND ID_PODCAST = '".$_SESSION['podcast']."' AND ID_CATEGORY ='".$category."' UNION ALL SELECT REIHENF FROM links WHERE ID_EPISODE = '".$_SESSION['cur_episode']."' AND ID_PODCAST = '".$_SESSION['podcast']."' AND ID_CATEGORY ='".$category."') AS QUERY";
+		$sql_select_max_reihenf = "SELECT MAX(REIHENF) AS MAX FROM (SELECT REIHENF FROM ".DB_PREFIX."topics WHERE ID_EPISODE = '".$_SESSION['cur_episode']."' AND ID_PODCAST = '".$_SESSION['podcast']."' AND ID_CATEGORY ='".$category."' UNION ALL SELECT REIHENF FROM ".DB_PREFIX."links WHERE ID_EPISODE = '".$_SESSION['cur_episode']."' AND ID_PODCAST = '".$_SESSION['podcast']."' AND ID_CATEGORY ='".$category."') AS QUERY";
  		$sql_select_max_result = mysqli_query($con, $sql_select_max_reihenf);
 		$row = mysqli_fetch_assoc($sql_select_max_result);
 		$row_new = 1 + $row['MAX'];		
